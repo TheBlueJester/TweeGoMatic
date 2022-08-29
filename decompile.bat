377a622daf5b98de2f29a29eb9ddc922e504a15b
Michael <80931844+TheBlueJester@users.noreply.github.com>
2022-08-29 21:52:43 +1000

    Update decompile.bat

--- decompile.bat
+++ decompile.bat
@@ -4,7 +4,7 @@ echo %1
 echo Now loading tweego binary from "%~dp0bin\tweego.exe"
 echo.
 echo The loaded Twee file will now be decompiled
-"%~dp0bin\tweego.exe" -d -o %~n1.twee %~n1.html
+"%~dp0bin\tweego.exe" -d -o "%~n1.twee" "%~n1.html"
 echo ...
 echo Twee file has now been decompiled
 echo Press Enter to exit.
